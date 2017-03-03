6be37b2d5b8b3bd1784432d7761c5ea502c39ab2
mixnix <nielipinskim@gmail.com>
2017-03-03 10:41:11 +0100

    splitting String into lines and then into chars

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.io.File;
  * Created by user_name on 01.03.2017.
  */
 public class Main {
-    private String level = "##########\n" +
+    private static String level = "##########\n" +
             "#        #\n" +
             "#        #\n" +
             "#        #\n" +
@@ -30,7 +30,14 @@ public class Main {
 
             Document doc = dBuilder.newDocument();
 
+            String[] lines = level.split("\\r\\n|\\r|\\n");
+            int num_of_lines = lines.length;
 
+            for(int n = 0; n < num_of_lines; ++n){
+                for(int i = 1; i <= lines[n].length(); ++i){
+                    //here converting to xml elements will beK
+                }
+            }
 
 
 
@@ -47,4 +54,7 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+
+
 }
